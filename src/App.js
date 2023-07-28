edab5d4446aee23eb1bb253b8fea7ef1525f6aa3
Pramod Yadav <pramodyadavpy2711@gmail.com>
2023-07-28 17:52:23 +0530

    added functionanlity to the form  for capture data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,14 +23,31 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(event) {
     event.preventDefault();
+
+    // if there's no description entered on the page then the page wouldn't submit the data and immediately return nothing
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // setting both description and quantity to their initail state after the data hass been captured
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        // these are controlled elements, value change recorder by onChange method on the target element
+        value={quantity}
+        onChange={(event) => setQuantity(Number(event.target.value))}
+      >
         {/* important method for creating array */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -35,7 +55,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
       <button>Add</button>
     </form>
   );
